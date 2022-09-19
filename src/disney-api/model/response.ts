d100cec66da9201e63448cd70c85a6dffbc6726b
Alec Custer <alcuster@icloud.com>
2022-09-19 13:34:01 -0700

    breakfast typo

--- src/disney-api/model/response.ts
+++ src/disney-api/model/response.ts
@@ -29,7 +29,7 @@ export interface AvailabilityResponse {
 
 // Disney obfuscates their "meal periods" (breakfast/brunch/lunch/dinner) with these weird ids
 export const mealPeriods = {
-  breakast: 80000712, //
+  breakfast: 80000712, //
   brunch: 80000713,
   lunch: 80000717,
   dinner: 80000714,
