888d0930dc33f62958f385aeb77a138f4bbca822
Alec Custer <alcuster@icloud.com>
2026-02-25 08:56:47 -0800

    Add release workflow and update version checks

--- __tests__/cli-integration.test.ts
+++ __tests__/cli-integration.test.ts
@@ -1,15 +1,16 @@
 const { system, filesystem } = require('gluegun');
 
 const src = filesystem.path(__dirname, '..');
+const { version } = require('../package.json');
 
 const cli = async (cmd) => system.run('node ' + filesystem.path(src, 'bin', 'dine-at-disney') + ` ${cmd}`);
 
 test('outputs version', async () => {
   const output = await cli('--version');
-  expect(output).toContain('0.0.1');
+  expect(output).toContain(version);
 });
 
 test('outputs help', async () => {
   const output = await cli('--help');
-  expect(output).toContain('0.0.1');
+  expect(output).toContain('dine-at-disney');
 });
